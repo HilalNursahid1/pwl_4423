0d885c5145d846f1b7f997d451b665d8254bacdb
HilalNursahid1 <82947133+HilalNursahid1@users.noreply.github.com>
2021-04-21 19:02:09 +0700

    Delete biodata.php

--- biodata.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo("Nama           : Hilal Nursahid") ; echo ("<br>");
-    echo("Umur           : 20 tahun "); echo("<br>");
-    echo("Alamat         : Komplek Tarubudaya");echo("<br>");
-    echo("Kota kelahiran : Semarang "); echo("<br>");
-    echo("Makanan fav.   : Pizza "); echo("<br>");
-    echo("Minuman fav.   : Susu "); echo("<br>");
-    echo("Hobi           : Game "); echo("<br>");
-?> 
